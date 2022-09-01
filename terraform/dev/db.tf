2fb500de72510b3f02a4c05ae74b2069b92f9896
jinchuantang <jinchuant@gmail.com>
2022-09-01 15:36:41 -0700

    add rds

---
+++ terraform/dev/db.tf
@@ -0,0 +1,35 @@
+# db.tf
+
+resource "aws_db_subnet_group" "data_subnet" {
+  name                   = "data-subnet"
+  subnet_ids             = module.network.aws_subnet_ids.data.ids
+
+  tags = var.common_tags
+}
+
+# SQL Server
+resource "aws_db_instance" "mssql" {
+  allocated_storage = 10
+  max_allocated_storage = 100
+  engine = "sqlserver-web"
+  instance_class = "db.t3.small"
+  name = "ceu-mssql"
+  username = local.db_creds.username
+  password = local.db_creds.password
+  skip_final_snapshot = true
+  backup_retention_period = 5
+  db_subnet_group_name = aws_db_subnet_group.data_subnet.name
+  kms_key_id = aws_kms_key.workbc-jb-kms-key.arn
+  vpc_security_group_ids  = [data.aws_security_group.data.id]
+}
+
+# create this manually
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "workbc-jb-db-creds"
+}
+
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
