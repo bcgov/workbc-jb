7dc3f410b42e968704ca090424c2f3d001e2c0a5
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-01 15:45:11 -0700

    Update db.tf

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -1,4 +1,4 @@
-# db.tf
+# RDS
 
 resource "aws_db_subnet_group" "data_subnet" {
   name                   = "data-subnet"
@@ -19,7 +19,7 @@ resource "aws_db_instance" "mssql" {
   skip_final_snapshot = true
   backup_retention_period = 5
   db_subnet_group_name = aws_db_subnet_group.data_subnet.name
-  kms_key_id = aws_kms_key.workbc-jb-kms-key.arn
+  kms_key_id = data.aws_kms_key.workbc-jb-kms-key.arn
   vpc_security_group_ids  = [data.aws_security_group.data.id]
 }
 
