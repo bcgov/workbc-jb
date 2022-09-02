41534cada345e804c66083bf5fea91e6f1a6f698
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-02 08:29:36 -0700

    Update db.tf

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -9,18 +9,18 @@ data "aws_db_subnet_group" "data_subnet" {
 
 # SQL Server
 resource "aws_db_instance" "mssql" {
-  allocated_storage = 10
-  max_allocated_storage = 100
-  engine = "sqlserver-web"
-  instance_class = "db.t3.small"
-  name = "ceu-mssql"
-  username = local.db_creds.username
-  password = local.db_creds.password
-  skip_final_snapshot = true
+  allocated_storage       = 10
+  max_allocated_storage   = 100
+  engine                  = "sqlserver-web"
+  instance_class          = "db.t3.small"
+  identifier              = "ceu-mssql"
+  username                = local.db_creds.username
+  password                = local.db_creds.password
+  skip_final_snapshot     = true
   backup_retention_period = 5
-  db_subnet_group_name = data.aws_db_subnet_group.data_subnet.name
-  kms_key_id = data.aws_kms_key.workbc-jb-kms-key.arn
-  storage_encrypted = true
+  db_subnet_group_name    = data.aws_db_subnet_group.data_subnet.name
+  kms_key_id              = data.aws_kms_key.workbc-jb-kms-key.arn
+  storage_encrypted       = true
   vpc_security_group_ids  = [data.aws_security_group.data.id]
 }
 
