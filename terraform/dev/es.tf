b4e886df6160fb35bfb2b774a89a71ca0705aea1
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-15 15:36:17 -0700

    Update es.tf

--- terraform/dev/es.tf
+++ terraform/dev/es.tf
@@ -17,6 +17,7 @@ resource "aws_elasticsearch_domain" "workbc-jb-cluster" {
 	
 	domain_endpoint_options {
 		enforce_https = true
+		tls_security_policy = Policy-Min-TLS-1-2-2019-07
 	}
 	
 	advanced_security_options {
