7e78979ee330627fd09b88a04fd076bb1b1269e7
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-16 11:22:02 -0700

    Update es.tf

--- terraform/dev/es.tf
+++ terraform/dev/es.tf
@@ -16,7 +16,10 @@ resource "aws_elasticsearch_domain" "workbc-jb-cluster" {
 	}
 	
 	vpc_options {
-		subnet_ids = module.network.aws_subnet_ids.app
+		subnet_ids = [
+			module.network.aws_subnet_ids.app[0].id,
+			module.network.aws_subnet_ids.app[1].id
+		]
 
 		security_group_ids = [aws_security_group.es_security_group.id]
 	}
