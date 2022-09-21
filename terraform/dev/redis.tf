d67840d972c09c48ed57e3a71e8ff34563d86c6b
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-21 09:17:06 -0700

    Update redis.tf

--- terraform/dev/redis.tf
+++ terraform/dev/redis.tf
@@ -2,13 +2,13 @@
 
 resource "aws_elasticache_replication_group" "jb_redis_rg" {
 	automatic_failover_enabled	=	true
-	preferred_cache_cluster_azs	=	[ca-central-1a, ca-central-1b]
+	preferred_cache_cluster_azs	=	["ca-central-1a", "ca-central-1b"]
 	replication_group_id		=	"jb-rep-group"
-	description					=	"Redis replication group for JobBoard"
-	node_type					=	"cache.t2.micro"
-	num_cache_clusters			=	2
+	description			=	"Redis replication group for JobBoard"
+	node_type			=	"cache.t2.micro"
+	num_cache_clusters		=	2
 	parameter_group_name		=	"default.redis6.x"
-	port						=	6379
+	port				=	6379
 	
 	lifecycle {
 		ignore_changes	=	[num_cache_clusters]
