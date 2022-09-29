17e82c434ed10a5c6ad7d18e02ce83900a5cb499
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-09-29 09:28:38 -0700

    Update ecs.tf

--- terraform/dev/ecs.tf
+++ terraform/dev/ecs.tf
@@ -154,7 +154,7 @@ resource "aws_ecs_task_definition" "admin-app" {
   
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    cpu_architecture        = "X86_64"
   }
 }
 
