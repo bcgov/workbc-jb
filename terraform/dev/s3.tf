9e91a470bb16806701cfe3223750bb1ef232cb85
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-10-21 12:50:51 -0700

    Update s3.tf

--- terraform/dev/s3.tf
+++ terraform/dev/s3.tf
@@ -9,35 +9,45 @@ resource "aws_s3_bucket_acl" "workbc_s3_acl" {
 
 resource "aws_s3_bucket_policy" "allow_access_from_other_accounts" {
   bucket = aws_s3_bucket.workbc_jb_s3.id
-  policy = jsonencode({
-    "Statement": [
-        {
-            "Principal": {
-                "AWS": "arn:aws:iam::054099626264:role/mssqlNativeBackupRestoreRole"
-            },
-			"Action": [
-                "s3:ListBucket",
-                "s3:GetBucketLocation"
-            ],
-            "Effect": "Allow",
-            "Resource": "arn:aws:s3:::workbc-dev-bucket"
-        },
-        {
-            "Principal": {
-                "AWS": "arn:aws:iam::054099626264:role/mssqlNativeBackupRestoreRole"
-            },
-			"Action": [
-                "s3:GetObjectAttributes",
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:ListMultipartUploadParts",
-                "s3:AbortMultipartUpload"
-            ],
-            "Effect": "Allow",
-            "Resource": "arn:aws:s3:::workbc-dev-bucket/*"
-        }
-    ],
-    "Version": "2012-10-17"
-  })
+  policy = data.aws_iam_policy_document.allow_access_from_other_accounts.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_other_accounts" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::054099626264:role/mssqlNativeBackupRestoreRole"]
+    }
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.workbc_jb_s3.arn}/*",
+    ]
+  }
+	
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::054099626264:role/mssqlNativeBackupRestoreRole"]
+    }
+
+    actions = [
+      "s3:GetObjectAttributes",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.workbc_jb_s3.arn}/*",
+    ]
+  }
 }
 
