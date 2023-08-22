a6911046f60d9607d5b6bc35cf8ad2882a3a97de
Guo <Pearl.Guo@gov.bc.ca>
2023-08-22 10:54:56 -0700

    Revert "Merge pull request #62 from bcgov/jon/WBCAMS-23b"
    This reverts commit 9a7f0d477fc3edd1c340939c2c1138cd6f7fe0b2, reversing
    changes made to 086016756715c28ee62a9a3828ccd88d07fc7260.

--- src/WorkBC.Web/ClientApp/projects/jb-lib/src/lib/components/item/item.component.ts
+++ src/WorkBC.Web/ClientApp/projects/jb-lib/src/lib/components/item/item.component.ts
@@ -58,6 +58,8 @@ export class ItemComponent {
         const offSetMinutes = offSet % 60;
         today.setHours(offSetHours, offSetMinutes, 0, 0);
         const expireDate = new Date(this.item.ExpireDate);
+        //expireDate.setHours(0, 0, 0, 0);
+        console.log(expireDate, today);
         result = expireDate <= today;
       }
     }
