91bb1bcc0f670f340c5a94f9dea030e668e8dc2b
Jonathan Longe <jlonge@corebox.net>
2024-01-04 13:42:26 -0800

    [WBCAMS-293] fix typo in SQL

--- src/WorkBC.Data/Migrations/20231222224412_UpdateQueenCharlotteCityName.cs
+++ src/WorkBC.Data/Migrations/20231222224412_UpdateQueenCharlotteCityName.cs
@@ -8,34 +8,34 @@ namespace WorkBC.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("UPDATE Locations" +
-                                 "SET City = 'Daajing Giids ( Queen Charlotte City )', " +
-                                 "Label = 'Daajing Giids ( Queen Charlotte City )'" +
+            migrationBuilder.Sql("UPDATE Locations " +
+                                 "SET [City] = 'Daajing Giids ( Queen Charlotte City )', " +
+                                 "[Label] = 'Daajing Giids ( Queen Charlotte City )'" +
                                  "WHERE LocationId = '1594'");
 
-            migrationBuilder.Sql("UPDATE ImportedJobsFederal" +
-                                 "SET ReIndexNeeded = 1" +
-                                 "WHERE JobId IN (SELECT JobId FROM Jobs where City = 'Queen Charlotte City')");
+            migrationBuilder.Sql("UPDATE ImportedJobsFederal " +
+                                 "SET [ReIndexNeeded] = 1" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
             
-            migrationBuilder.Sql("UPDATE ImportedJobsWanted" +
-                                 "SET ReIndexNeeded = 1" +
-                                 "WHERE JobId IN (SELECT JobId FROM Jobs where City = 'Queen Charlotte City')");
+            migrationBuilder.Sql("UPDATE ImportedJobsWanted " +
+                                 "SET [ReIndexNeeded] = 1" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("UPDATE Locations" +
-                                 "SET City = 'Queen Charlotte City', " +
-                                 "Label = 'Queen Charlotte City'" +
+            migrationBuilder.Sql("UPDATE Locations " +
+                                 "SET [City] = 'Queen Charlotte City', " +
+                                 "[Label] = 'Queen Charlotte City'" +
                                  "WHERE LocationId = '1594'");
             
-            migrationBuilder.Sql("UPDATE ImportedJobsFederal" +
-                                 "SET ReIndexNeeded = 0" +
-                                 "WHERE JobId IN (SELECT JobId FROM Jobs where City = 'Queen Charlotte City')");
+            migrationBuilder.Sql("UPDATE ImportedJobsFederal " +
+                                 "SET [ReIndexNeeded] = 0" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
             
-            migrationBuilder.Sql("UPDATE ImportedJobsWanted" +
-                                 "SET ReIndexNeeded = 0" +
-                                 "WHERE JobId IN (SELECT JobId FROM Jobs where City = 'Queen Charlotte City')");
+            migrationBuilder.Sql("UPDATE ImportedJobsWanted " +
+                                 "SET [ReIndexNeeded] = 0" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
             
         }
     }
--- src/WorkBC.Data/README.md
+++ src/WorkBC.Data/README.md
@@ -3,7 +3,11 @@
 
 - In your development environment, install the migration tools globally using: 
   `dotnet tool install --global dotnet-ef --version 6.*`
-- Change directory to: `.\src\WorkBC.Web`
+- Change directory to: `.\src`
 - List the possible database contexts (required below): `dotnet-ef dbcontext list`
-- Create a new migration using: `dotnet ef migrations add <SomeMigrationName> --context JobBoardContext --project ..\WorkBC.Data`
+- Create a new migration using: `dotnet ef migrations add <SomeMigrationName> --project WorkBC.Data --startup-project WorkBC.Web --context JobBoardContext`
+
+### To test the migration locally
+- Change directory to: `.\src`
+- Run the migration using: `dotnet ef database update --project WorkBC.Data --startup-project WorkBC.Web --context JobBoardContext`
 
