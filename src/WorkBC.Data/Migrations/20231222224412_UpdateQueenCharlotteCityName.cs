0a1a7247a7be7e00834b0cca5a860cc7b316cdbc
Jonathan Longe <jlonge@corebox.net>
2024-01-04 14:59:58 -0800

    [WBCAMS-293] update queen charlotte city name (#108)

---
+++ src/WorkBC.Data/Migrations/20231222224412_UpdateQueenCharlotteCityName.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkBC.Data.Migrations
+{
+    public partial class UpdateQueenCharlotteCityName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Locations " +
+                                 "SET [City] = 'Daajing Giids ( Queen Charlotte City )', " +
+                                 "[Label] = 'Daajing Giids ( Queen Charlotte City )'" +
+                                 "WHERE LocationId = '1594'");
+
+            migrationBuilder.Sql("UPDATE ImportedJobsFederal " +
+                                 "SET [ReIndexNeeded] = 1" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
+            
+            migrationBuilder.Sql("UPDATE ImportedJobsWanted " +
+                                 "SET [ReIndexNeeded] = 1" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Locations " +
+                                 "SET [City] = 'Queen Charlotte City', " +
+                                 "[Label] = 'Queen Charlotte City'" +
+                                 "WHERE LocationId = '1594'");
+            
+            migrationBuilder.Sql("UPDATE ImportedJobsFederal " +
+                                 "SET [ReIndexNeeded] = 0" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
+            
+            migrationBuilder.Sql("UPDATE ImportedJobsWanted " +
+                                 "SET [ReIndexNeeded] = 0" +
+                                 "WHERE JobId IN (SELECT [JobId] FROM Jobs where City = 'Queen Charlotte City')");
+            
+        }
+    }
+}
