b82ecd7af24a750e4bf7e84d75092ef93579f051
Guo <Pearl.Guo@gov.bc.ca>
2024-03-18 16:21:30 -0700

    first commit of jb2

---
+++ terraform/dev2/local.tf
@@ -0,0 +1,8 @@
+
+locals {
+  common_tags        = var.common_tags
+  create_ecs_service = var.app_version == "" ? 0 : 1
+  df_conn			       = "Server=${aws_db_instance.mssql.address};Database=WorkBC_JobBoard;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
+  ent_conn           = "Server=${aws_db_instance.mssql.address};Database=WorkBC_Enterprise;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
+  es_conn            = "https://${aws_elasticsearch_domain.workbc-jb2-cluster.endpoint}" 
+}
