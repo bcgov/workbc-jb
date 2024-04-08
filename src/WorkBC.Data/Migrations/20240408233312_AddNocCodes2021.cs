a0cd20a3eea3f681caaf7d6a14342ae26638d8ac
SunanditaSahu <sunanditasahu@gmail.com>
2024-04-08 16:34:46 -0700

    Changes for https://hive.aved.gov.bc.ca/jira/browse/NOC-376 Created a new JobBoard.NocCodes2021 table with the same schema as NocCodes, except that the Code model is a 5-digit string.

---
+++ src/WorkBC.Data/Migrations/20240408233312_AddNocCodes2021.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkBC.Data.Migrations
+{
+    public partial class AddNocCodes2021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NocCodes2021",
+                columns: table => new
+                {
+                    Id = table.Column<short>(type: "smallint", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    FrenchTitle = table.Column<string>(type: "nvarchar(180)", maxLength: 180, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NocCodes2021", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NocCodes2021");
+        }
+    }
+}
