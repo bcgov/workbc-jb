ff83063e9d65a22520547555540e9e950db422df
Sunandita Sahu <sunanditasahu@gmail.com>
2024-04-11 13:04:53 -0700

    Changes for https://hive.aved.gov.bc.ca/jira/browse/NOC-376
    Created a new JobBoard.NocCodes2021 table with the same schema as NocCodes, except that the Code model is a 5-digit string.

--- src/WorkBC.Data/Migrations/20240411194800_AddNocCodes2021.Designer.cs
+++ src/WorkBC.Data/Migrations/20240411194800_AddNocCodes2021.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkBC.Data;
 
@@ -11,9 +12,10 @@ using WorkBC.Data;
 namespace WorkBC.Data.Migrations
 {
     [DbContext(typeof(JobBoardContext))]
-    partial class JobBoardContextModelSnapshot : ModelSnapshot
+    [Migration("20240411194800_AddNocCodes2021")]
+    partial class AddNocCodes2021
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
