71769d26b84aabc24a60c6e694e61fe2f4be03f9
Sunandita Sahu <sunanditasahu@gmail.com>
2024-04-15 16:45:47 -0700

    NOC-378: Added a foreign key [NocCodeId2021] to both Jobs and JobVersions tables. It should default to NULL.

--- src/WorkBC.Data/Migrations/20240415225803_AddFKNoCCode2021.cs
+++ src/WorkBC.Data/Migrations/20240415225803_AddFKNoCCode2021.cs
@@ -9,24 +9,58 @@ namespace WorkBC.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             //Add new columns to dbo.Jobs and dbo.JobVersions tables.
-            migrationBuilder.AddColumn<string>(
+            migrationBuilder.AddColumn<int>(
                name: "NocCodeId2021",
                table: "Jobs",
-               type: "nvarchar(5)",
+               type: "int",
                maxLength: 5,
                nullable: true);
 
-            migrationBuilder.AddColumn<string>(
+            migrationBuilder.AddColumn<int>(
                 name: "NocCodeId2021",
                 table: "JobVersions",
-                type: "nvarchar(5)",
+                type: "int",
                 maxLength: 5,
                 nullable: true);
 
+            //// Populate new NocCodeId2021 column in the Jobs table
+            //migrationBuilder.Sql(@"INSERT INTO [dbo].[Jobs]
+            //           ([NocCodeId2021])
+            //     SELECT nc.Code FROM [dbo].[Jobs] j INNER JOIN NocCodes2021 nc ON j.NocCodeId2021 = nc.Id;");
+
+            ////Populate new NocCodeId2021 column in the JobVersions table
+            //migrationBuilder.Sql(@"INSERT INTO [dbo].[JobVersions]
+            //           ([NocCode])
+            //     SELECT nc.Code FROM [dbo].[JobVersions] j INNER JOIN NocCodes nc ON j.NocCodeId2021 = nc.Id;");
+
+            //Create new foreign keys
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_NocCodes2021_NocCodeId2021",
+                table: "Jobs",
+                column: "NocCodeId2021",
+                principalTable: "NocCodes2021",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobVersions_NocCodes2021_NocCodeId2021",
+                table: "JobVersions",
+                column: "NocCodeId2021",
+                principalTable: "NocCodes2021",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_NocCodes2021_NocCodeId2021",
+                table: "Jobs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobVersions_NocCodes2021_NocCodeId2021",
+                table: "JobVersions");
+
             migrationBuilder.DropColumn(
                 name: "NocCodeId2021",
                 table: "Jobs");
@@ -34,6 +68,8 @@ namespace WorkBC.Data.Migrations
             migrationBuilder.DropColumn(
                 name: "NocCodeId2021",
                 table: "JobVersions");
+
+
         }
     }
 }
