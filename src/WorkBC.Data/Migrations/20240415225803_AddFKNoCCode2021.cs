de6dbe92e63020600952efeebcc22a6259602fc0
Sunandita Sahu <sunanditasahu@gmail.com>
2024-04-15 16:24:56 -0700

    Changes for NOC-378: Add new column 'NocCodeId2021' in dbo.Jobs and dbo.JobVersions tables.

---
+++ src/WorkBC.Data/Migrations/20240415225803_AddFKNoCCode2021.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkBC.Data.Migrations
+{
+    public partial class AddFKNoCCode2021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Add new columns to dbo.Jobs and dbo.JobVersions tables.
+            migrationBuilder.AddColumn<string>(
+               name: "NocCodeId2021",
+               table: "Jobs",
+               type: "nvarchar(5)",
+               maxLength: 5,
+               nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NocCodeId2021",
+                table: "JobVersions",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: true);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NocCodeId2021",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "NocCodeId2021",
+                table: "JobVersions");
+        }
+    }
+}
