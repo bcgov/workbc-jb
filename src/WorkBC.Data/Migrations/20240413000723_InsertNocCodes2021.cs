4500a652df5266c00e38cc90a04ea9df86b93cf2
Sunandita Sahu <sunanditasahu@gmail.com>
2024-04-15 12:49:05 -0700

    Implemented the review comments.

--- src/WorkBC.Data/Migrations/20240413000723_InsertNocCodes2021.cs
+++ src/WorkBC.Data/Migrations/20240413000723_InsertNocCodes2021.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 using System.Text.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System;
-using WorkBC.Data.Model.JobBoard;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using WorkBC.Data.Model.Enterprise;
 using System.IO;
-using Microsoft.VisualBasic;
 using System.Text;
-using System.Linq;
 
 #nullable disable
 
@@ -33,51 +27,24 @@ namespace WorkBC.Data.Migrations
         }
         protected async override void Up(MigrationBuilder migrationBuilder)
         {
-            Console.WriteLine("Hello");
-            // Append to NocCodes2021  table from SSOT API
             var allNocs = await GetNocCodes2021(_ssotUrl);
-
-            Console.WriteLine("allNocs: " + allNocs.Count.ToString());
-            Console.WriteLine("Inside Up method");
-
-            //migrationBuilder.Sql(@"INSERT INTO [dbo].[NocCodes2021]
-            //           ([Id],[Code],[Title], [FrenchTitle])
-            //     VALUES
-            //           (1, '1', 'test1, test2', 'testFr1')");
-
 
             foreach (var nocs in allNocs)
             {
 
-                if (String.IsNullOrEmpty(nocs.noc_2021))
+                if (!String.IsNullOrEmpty(nocs.noc_2021))
                 {
-                    Console.WriteLine("null noc");
-                }
-                else
-                {
-                    try
-                    {
                         int.TryParse(nocs.noc_2021, out int id);
 
                         string code = nocs.noc_2021.ToString();
                         string title = nocs.label_en.ToString();
                         string frenchTitle = nocs.label_fr.ToString();
 
-                        //Console.WriteLine("id: " + id);
-                        //Console.WriteLine("code: " + code);
-                        //Console.WriteLine("title: " + title);
-                        //Console.WriteLine("frenchTitle: " + frenchTitle);
-
                         migrationBuilder.InsertData(
                             table: "NocCodes2021",
                             columns: new[] { "Id", "Code", "Title", "FrenchTitle" },
                             values: new object[] { id, code, title, frenchTitle });
 
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Inside catch in up: " + ex.Message.ToString());
-                    }
                 }
 
 
@@ -87,7 +54,6 @@ namespace WorkBC.Data.Migrations
 
         protected async override void Down(MigrationBuilder migrationBuilder)
         {
-            Console.WriteLine("Inside Insert Down method");
             //Truncate table- delete all records
             migrationBuilder.Sql(@"TRUNCATE TABLE [dbo].[NocCodes2021]");
         }
@@ -95,8 +61,7 @@ namespace WorkBC.Data.Migrations
         {
 
             List<NocCodeSSOT> newSSOTCodes = new List<NocCodeSSOT>();
-            try
-            {
+
                 using (var _httpClient = new HttpClient())
                 {
                     var response = await _httpClient.GetAsync(nocCodes2021SSOTUrl);
@@ -107,30 +72,29 @@ namespace WorkBC.Data.Migrations
                     if (response.IsSuccessStatusCode)
                     {
                         var jsonString = await response.Content.ReadAsStringAsync();
-                        Console.WriteLine("Inside GetNoc method");
 
                         // convert string to stream
                         byte[] byteArray = Encoding.UTF8.GetBytes(jsonString);
                         //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
                         MemoryStream jsonStream = new MemoryStream(byteArray);
-                        Console.WriteLine("After stream method");
+                       
                         newSSOTCodes = await JsonSerializer.DeserializeAsync<List<NocCodeSSOT>>(jsonStream);
 
-                        Console.WriteLine("After deserialize method");
                     }
 
                 }
 
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Inside catch" + ex.Message.ToString());
-            }
             return newSSOTCodes;
 
 
         }
+
+    }
+    public class NocCodeSSOT
+    {
+        public string noc_2021 { get; set; }
+        public string label_en { get; set; }
+        public string label_fr { get; set; }
 
     }
 }
