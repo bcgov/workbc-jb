4fa5b89f22b26f0e119c3979fd51c727a6afa995
Sunandita Sahu <sunanditasahu@gmail.com>
2024-05-13 16:11:43 -0700

    NOC-367: At src/WorkBC.Web/Controllers/CareerProfilesController.cs, replaced every usage of EnterpriseContext with its equivalent tables in JobBoardContext in GetSavedCareerProfiles method.

--- src/WorkBC.Data/Model/JobBoard/SavedCareerProfile.cs
+++ src/WorkBC.Data/Model/JobBoard/SavedCareerProfile.cs
@@ -11,8 +11,8 @@ namespace WorkBC.Data.Model.JobBoard
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Column("EDM_CareerProfile_CareerProfileId")]
-        public int CareerProfileId { get; set; }
+        [Column("NocCodeId2021")]
+        public int NocCodeId2021 { get; set; }
 
         [ForeignKey("JobSeeker")]
         public string AspNetUserId { get; set; }
