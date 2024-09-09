979d82c72ca77a5d2b7e4c53f7d4287c9d64415f
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-09-09 13:19:30 -0700

    Add files via upload

---
+++ src/WorkBC.Data/Migrations/20240909201041_UpdateLockoutEndDatatype.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace WorkBC.Data.Migrations
+{
+    public partial class UpdateLockoutEndDatatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LockoutEnd",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: true,
+                oldType: "datetimeoffset"); 
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
