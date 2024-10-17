7748715e5b193beaa3497e94f146e5e63ccd5f7c
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2024-10-17 09:57:39 -0700

    Update db.tf

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -52,3 +52,35 @@ locals {
     data.aws_secretsmanager_secret_version.creds.secret_string
   )
 }
+
+resource "aws_rds_cluster" "postgres" {
+  cluster_identifier      = "jb-postgres-cluster"
+  engine                  = "aurora-postgresql"
+  engine_version          = "16.4"
+# engine_mode		  = "provisioned"
+  master_username         = local.db_creds.username
+  master_password         = local.db_creds.password
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  db_subnet_group_name    = aws_db_subnet_group.data_subnet.name
+  kms_key_id              = aws_kms_key.workbc-jb-kms-key.arn
+  storage_encrypted       = true
+  vpc_security_group_ids  = [data.aws_security_group.data.id]
+  skip_final_snapshot     = true
+  final_snapshot_identifier = "jb-finalsnapshot"
+  
+  serverlessv2_scaling_configuration {
+    max_capacity = 2.0
+    min_capacity = 1.0
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_rds_cluster_instance" "postgres" {
+  count = 2
+  cluster_identifier = aws_rds_cluster.postgres.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.postgres.engine
+  engine_version     = aws_rds_cluster.postgres.engine_version
+}
