c93192cbd2bf44aa5c89471de529fc1912abd811
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-01-09 09:32:43 -0800

    Update local.tf

--- terraform/dev/local.tf
+++ terraform/dev/local.tf
@@ -5,4 +5,5 @@ locals {
   df_conn			       = "Server=jb-babelr1-instance-1.cchtvqxqwetj.ca-central-1.rds.amazonaws.com;Database=WorkBC_JobBoard;uid=${local.db_creds.username};pwd=${local.db_creds.babelpassword}"
   ent_conn           = "Server=${aws_db_instance.mssql.address};Database=WorkBC_Enterprise;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
   es_conn            = "https://${aws_elasticsearch_domain.workbc-jb-cluster.endpoint}" 
+
 }
