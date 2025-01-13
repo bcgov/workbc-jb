e36b27151742b511a6bccb06c6fdb0260b70e67a
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-01-13 14:11:25 -0800

    Update local.tf

--- terraform/dev2/local.tf
+++ terraform/dev2/local.tf
@@ -2,7 +2,7 @@
 locals {
   common_tags        = var.common_tags
   create_ecs_service = var.app_version == "" ? 0 : 1
-  df_conn			       = "Server=${data.aws_db_instance.mssql.address};Database=WorkBC_JobBoard_NOC;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
+  df_conn			       = "Server=${data.aws_db_instance.mssql.address};Database=WorkBC_JobBoard;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
   ent_conn           = "Server=${data.aws_db_instance.mssql.address};Database=WorkBC_Enterprise_NOC;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
   es_conn            = "https://${aws_elasticsearch_domain.workbc-jb2-cluster.endpoint}" 
 }
