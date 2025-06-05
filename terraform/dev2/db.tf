6dcf668c7b446dd5b87b06b643b54beda8434097
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-06-05 13:51:05 -0700

    Update db.tf

--- terraform/dev2/db.tf
+++ terraform/dev2/db.tf
@@ -25,28 +25,28 @@ resource "aws_db_option_group" "mssql-og" {
 
 # SQL Server
 #TODO after db creation
-data "aws_db_instance" "mssql" {
-  db_instance_identifier = "ceu-mssql"
-  /*identifier              = "ceu-mssql"
-  allocated_storage       = 300
-  max_allocated_storage   = 600
-  engine                  = "sqlserver-web"
-  engine_version	  = "15.00"
-  instance_class          = "db.t3.small"
-  username                = local.db_creds.username
-  password                = local.db_creds.password
-  skip_final_snapshot     = true
-  backup_retention_period = 5
-  db_subnet_group_name    = data.aws_db_subnet_group.data_subnet.name
+#data "aws_db_instance" "mssql" {
+#  db_instance_identifier = "ceu-mssql"
+#  /*identifier              = "ceu-mssql"
+#  allocated_storage       = 300
+#  max_allocated_storage   = 600
+#  engine                  = "sqlserver-web"
+#  engine_version	  = "15.00"
+#  instance_class          = "db.t3.small"
+#  username                = local.db_creds.username
+#  password                = local.db_creds.password
+#  skip_final_snapshot     = true
+#  backup_retention_period = 5
+#  db_subnet_group_name    = data.aws_db_subnet_group.data_subnet.name
 #TODO after db creation
-  kms_key_id              = data.aws_kms_key.workbc-jb-kms-key.arn
-  storage_encrypted       = true
-  vpc_security_group_ids  = [data.aws_security_group.data.id, aws_security_group.mssql_security_group.id]
-  option_group_name	  = aws_db_option_group.mssql-og.name
-  timezone		  = "Pacific Standard Time"
-  apply_immediately	  = true
-  */
-}
+#  kms_key_id              = data.aws_kms_key.workbc-jb-kms-key.arn
+#  storage_encrypted       = true
+#  vpc_security_group_ids  = [data.aws_security_group.data.id, aws_security_group.mssql_security_group.id]
+#  option_group_name	  = aws_db_option_group.mssql-og.name
+#  timezone		  = "Pacific Standard Time"
+#  apply_immediately	  = true
+#  */
+#}
 
 # create this manually
 #TODO after db creation
