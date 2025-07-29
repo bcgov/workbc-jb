229078e6022539f9cc7d6a5e3aaf7459a54bb7d1
Sunandita Sahu <sunanditasahu@gmail.com>
2025-07-29 12:41:19 -0700

    Converted the SQL script to migration

--- src/scripts/sql/WBCAMS-1326_UpdateSystemSettings_addRel.sql
+++
@@ -1,3 +0,0 @@
-  Update [WorkBC_jobboard_dev].[dbo].[SystemSettings]
-  Set Value = 'The <a href="http://noc.esdc.gc.ca/" target="_blank" rel="noopener noreferrer" style="text-decoration: underline;">National Occupational Classification</a> system classifies all occupations in Canada.'
-  where Name ='shared.tooltips.nocCode'
