44fcde6f651552033a5e2194cec71e0b6ffda8eb
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-10-30 13:42:21 -0700

    Update db.tf

--- terraform/prod/db.tf
+++ terraform/prod/db.tf
@@ -97,13 +97,13 @@ resource "aws_rds_cluster_instance" "postgres_babelfish" {
   engine_version     = aws_rds_cluster.postgres_babelfish.engine_version
 }
 
-resource "aws_rds_cluster_instance" "postgres" {
-  count = 2
-  cluster_identifier = aws_rds_cluster.postgres.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.postgres.engine
-  engine_version     = aws_rds_cluster.postgres.engine_version
-}
+# resource "aws_rds_cluster_instance" "postgres" {
+#  count = 2
+#  cluster_identifier = aws_rds_cluster.postgres.id
+#  instance_class     = "db.serverless"
+#  engine             = aws_rds_cluster.postgres.engine
+#  engine_version     = aws_rds_cluster.postgres.engine_version
+#}
 
 resource "aws_rds_cluster" "postgres_jbnewfinal" {
 	cluster_identifier = "jbnewfinal"
