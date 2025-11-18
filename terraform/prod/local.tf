5d1f91e7ac5c89314b61b6954efc226f5d43c2b0
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-11-18 09:45:41 -0800

    Update local.tf (#248)
    * Update local.tf
    
    * Update local.tf
    
    * Update local.tf
    
    * Update local.tf

--- terraform/prod/local.tf
+++ terraform/prod/local.tf
@@ -2,7 +2,7 @@
 locals {
   common_tags        = var.common_tags
   create_ecs_service = var.app_version == "" ? 0 : 1
-  df_conn			       = "Server=${aws_rds_cluster.postgres_babelfish.endpoint};Database=WorkBC_JobBoard;uid=${local.db_creds.username};pwd=${local.db_creds.babelpassword}"
+  df_conn			       = "Host=${aws_rds_cluster.postgres_jbnewfinal.endpoint};Database=WorkBC_JobBoard;UserName=${local.db_creds.username};Password=${local.db_creds.newjbpost}"
  # ent_conn           = "Server=${aws_db_instance.mssql.address};Database=WorkBC_Enterprise;uid=${local.db_creds.username};pwd=${local.db_creds.password}"
   es_conn            = "https://${aws_elasticsearch_domain.workbc-jb-cluster.endpoint}" 
 }
