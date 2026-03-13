a777c32d5c693504e41d623b3607e2b0770b37a4
Bidyashish <hi@bidyashish.com>
2026-03-13 10:23:40 -0700

    update

--- src/WorkBC.ElasticSearch.Indexing/Services/MIgrationService.cs
+++ src/WorkBC.ElasticSearch.Indexing/Services/MIgrationService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -25,6 +25,8 @@ namespace WorkBC.ElasticSearch.Indexing.Services
 
                 try
                 {
+                    // Allow up to 10 minutes for large schema migrations (e.g. ALTER COLUMN TYPE)
+                    _dbContext.Database.SetCommandTimeout(600);
                     _dbContext.Database.Migrate();
                     return true;
                 }
